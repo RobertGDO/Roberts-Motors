698f7af2a82d4305ddc89f96c0ba8ff6e708cc96
RobertGDO <47525776+RobertGDO@users.noreply.github.com>
2024-05-09 12:54:19 +0100

    Revert "Add files via upload"

--- Roberts-Motors/websites/default/templates/index.html.php
+++
@@ -1,6 +0,0 @@
-        <h2>Top 10 best selling cars</h2>
-        <p>Polo</p>
-        <img src="images/Polo.png" alt=" The picture is of the best discounted car.">
-        <p>Polo</p>
-        <img src="images/Polo.png" alt=" The picture is of the best discounted car.">
-    </main>
