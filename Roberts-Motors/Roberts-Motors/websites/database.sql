2c5db02c137d060d5b2d979221429f1c65b06223
roxie800 <126501575+roxie800@users.noreply.github.com>
2024-05-10 14:30:00 +0100

    Add files via upload
    added filter/refine by search into the aside on the grid layout
    added css to the hovering 
    changed padding for aside
    changed flex direction to column instead of row

---
+++ Roberts-Motors/Roberts-Motors/websites/database.sql
@@ -0,0 +1,90 @@
+-- MariaDB dump 10.19  Distrib 10.5.19-MariaDB, for Linux (x86_64)
+--
+-- Host: mysql    Database: roberts_motors
+-- ------------------------------------------------------
+-- Server version	11.2.2-MariaDB-1:11.2.2+maria~ubu2204
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `roberts_motors`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `roberts_motors` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `roberts_motors`;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cars` (
+  `car_id` int(11) NOT NULL AUTO_INCREMENT,
+  `price` decimal(8,2) NOT NULL,
+  `car_name` varchar(255) NOT NULL,
+  `engine` varchar(255) NOT NULL,
+  `details` varchar(255) NOT NULL,
+  `summary` varchar(255) NOT NULL,
+  `images` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`car_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` VALUES (1,1000.00,'Vauxhall','1.0','eco-flex','Reliable car on the road, 2004 reg','https://cdn-images.fleetnews.co.uk/thumbs/856x856/web-clean/1/root/img-0613.jpg'),(2,3000.00,'BMW','2.0','TDI','Very fast','https://www.stratstone.com/-/media/stratstone/blog/2020/bmw-m-the-history-of-bmw-m-cars/bmw-e46-m3-1280x720px.ashx?mh=1440&la=en&h=720&w=1280&mw=2560&hash=916A0255BF8482B4954A222291F99AFD');
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test` (
+  `idtest` int(11) NOT NULL,
+  PRIMARY KEY (`idtest`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `test`
+--
+
+LOCK TABLES `test` WRITE;
+/*!40000 ALTER TABLE `test` DISABLE KEYS */;
+/*!40000 ALTER TABLE `test` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'roberts_motors'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-10 12:50:29
---
+++ Roberts-Motors/Roberts-Motors/websites/default/templates/buycars.html.php
@@ -0,0 +1,45 @@
+
+
+<?php
+
+$pdo = new PDO('mysql:dbname=roberts_motors;host=mysql', 'student', 'student');
+
+$find = findAll($pdo, 'cars', 'car_id');
+
+
+?>
+
+<hr />
+
+<h2>Cars For Sale</h2>
+
+
+<?php
+echo '<ul class="CarProducts">';
+foreach($find as $cars){
+
+echo '<li>';
+?>
+    <img src=<?php echo $cars['images'];?> alt=<?php echo $cars['summary'];?>  width="200" 
+     height="200">
+
+<?php       
+        echo '<h3>' . $cars['price'].'</div>';
+
+        echo '<p>'. 'car_name:' . " " . $cars['car_name'].'</p>';
+
+        echo '<p>'. 'Engine:' . " " . $cars['engine'].'</p>';
+        
+        echo '<p>'. 'details' . " " . $cars['details'].'</h3>';
+
+        echo '<p>'. 'Summary: ' . " " . $cars['summary'].'</h3>';
+
+        if (isset($_SESSION['loggedin'])) {
+        ?>
+        <a href="Reviews.php?id=<?=$product['productid']?>"> Ask a question</a>
+        <?php
+        }
+echo '</li>';
+}
+echo '</ul>';
+?>
