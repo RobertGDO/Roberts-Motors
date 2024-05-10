05f588eb075af20c18e1be4181282fb773f33ca0
RobertGDO <47525776+RobertGDO@users.noreply.github.com>
2024-05-10 14:05:57 +0100

    Delete Roberts-Motors-1/Roberts-Motors directory

--- Roberts-Motors-1/Roberts-Motors/websites/default/templates/index.html.php
+++
@@ -1,8 +0,0 @@
-        <h1>Welcome to Robert Motors</h1>
-        <p>We stock a large variety of different branded cars</p>
-        <h2>Top 10 best selling cars: </h2>
-        <p>Polo</p>
-        <img src="images/Polo.png" alt=" The picture is of the best discounted car.">
-        <p>Polo</p>
-        <img src="images/Polo.png" alt=" The picture is of the best discounted car.">
-    </main>
