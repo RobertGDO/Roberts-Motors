31136293286d72e097fb669f2b186bdefa879225
roxie800 <126501575+roxie800@users.noreply.github.com>
2024-05-10 13:57:20 +0100

    Add files via upload
    added filter/refine by search to the aside bar
    reduced padding on the main
    changed percentage of grid in desktop.css

---
+++ Roberts-Motors-1/Roberts-Motors/websites/default/templates/shoppingcart.html.php
@@ -0,0 +1,57 @@
+
+        <div id="Introduction">
+            <p>At the beginning of this project, I was so worried to change something as I thought if I did it might
+                ruin the whole website and so I never used to delete anything only commented things out. Finally towards
+                the end of the project I had realised branches were exactly what I was missing and eventually I deleted
+                code I did not need anymore.</p>
+        </div>
+        <div id="Middle">
+            <p>I turned auto saver on for visual studio initially as it prevents data loss but soon realised this was
+                not good as it is time consuming and harder to recover unwanted changes. I used a specific colour scheme
+                which made the website look bold, consistent, clear and professional. I also changed the main grid only
+                on portfolio and site report webpages as the aside was just a blank space which was not needed.</p>
+        </div>
+        <div id="Middle2">
+            <p>Took me a long time to find how to get rid or add margin/padding spaces until I learnt how to use
+                inspect. I particularly like when I hover over sections in the html, it highlights them on the page so I
+                know exactly how to grid is laid out. Also shows quick changes in CSS code without making them permanent
+                like branches except with the terminal at the side of the webpage with direct implementation.</p>
+        </div>
+        <div id="Conculsion">
+            <p>Overall, with my experience in this project, I found this project took a lot of patience and, trial and
+                error but was fun. I had made sure to add many comments so that when I had come back to do some more
+                work, I knew exactly where I had left incomplete code to continue with later.</p>
+        </div>
+        <div>
+            <p>Week One Progress</p>
+            <p>Basic code for HTML</p>
+            <img src="images/BasicWebsiteCode.png" alt="Image of code in week one">
+            <p>Outcome without css</p>
+            <img src="images/BasicWebsiteContent.png" alt="Image of outcome in week one">
+        </div>
+        <div>
+            <p>Week Two Progress</p>
+            <p>Code for grid</p>
+            <img src="images/TemplateCode.png" alt="Image of code in week two">
+            <p>Outcome for grid</p>
+            <img src="images/TemplateOutcome.png" alt="Image of outcome in week two">
+        </div>
+        <div id="validation">
+            <p>Validation Section</p>
+        </div>
+        <p>Index Validation</p>
+        <img src="images/IndexValidation.png" alt="Image of index validation">
+        <p>Portfolio Validation</p>
+        <img src="images/PortfolioValidation.png" alt="Image of portfolio validation">
+        <p>Work Experience Validation</p>
+        <img src="images/WorkExperienceValidation.png" alt="Image of work experience validation">
+        <p>Site Report Validation</p>
+        <img src="images/SiteReportValidation.png" alt="Image of site report validation">
+        <p>Contact Validation</p>
+        <img src="images/ContactValidation.png" alt="Image of contact validation">
+        <p>Main Style Sheet Validation</p>
+        <img src="images/MainStyleSheet.png" alt="Image of main.css validation">
+        <p>Desktop Style Sheet Validation</p>
+        <img src="images/DesktopStyleSheet.png" alt="Image of desktop.css validation">
+        <p>Mobile Style Sheet Validation</p>
+        <img src="images/MobileStyleSheet.png" alt="Image of mobile.css validation">
