bc6b5b90fbe7fd8c0015191503d55313be094f8d
zacharyw2912 <zachary.white2912@gmail.com>
2024-05-11 12:22:42 +0100

    shopping cart semi functional

--- Roberts-Motors/websites/default/templates/buycars.html.php
+++ Roberts-Motors/websites/default/templates/buycars.html.php
@@ -1,47 +1,48 @@
-
-
 <?php
 
 session_start();
 
 $pdo = new PDO('mysql:dbname=roberts_motors;host=mysql', 'student', 'student');
-
 $find = findAll($pdo, 'cars', 'car_id');
 
+if(isset($_POST['add_to_cart'])){
+    $car_id = $_POST['car_id'];
+    $car_price = $_POST['car_price'];
+    $car_name = $_POST['car_name'];
 
+    $insert_product = $pdo->prepare('INSERT INTO cart (car_id, price, car_name) VALUES (:car_id, :car_price, :car_name)');
+    $insert_product->execute(['car_id' => $car_id, 'car_price' => $car_price, 'car_name' => $car_name]);
+}
 ?>
 
 <hr />
 
 <h2>Cars For Sale</h2>
 
-
 <?php
 echo '<ul class="CarProducts">';
 foreach($find as $cars){
-
-echo '<li>';
+    echo '<li>';
 ?>
-    <img src=<?php echo $cars['images'];?> alt=<?php echo $cars['summary'];?>  width="200" 
-     height="200">
+    <img src="<?php echo $cars['images'];?>" alt="<?php echo $cars['summary'];?>" width="200" height="200">
 
 <?php       
-        echo '<p>' . '£' . $cars['price'].'</div>';
-
-        echo '<p>'. 'Car Make:' . " " . $cars['car_name'].'</p>';
-
-        echo '<p>'. 'Engine:' . " " . $cars['engine'].'</p>';
-        
-        echo '<p>'. 'Details:' . " " . $cars['details'].'</h3>';
-
-        echo '<p>'. 'Summary: ' . " " . $cars['summary'].'</h3>';
-
-        if (isset($_SESSION['loggedin'])) {
-        ?>
-        <a href="Reviews.php?id=<?=$product['productid']?>"> View the Product</a>
-        <?php
-        }
-echo '</li>';
+    echo '<p>' . '£' . $cars['price'].'</p>';
+    echo '<p>'. 'Car Make:' . " " . $cars['car_name'].'</p>';
+    echo '<p>'. 'Engine:' . " " . $cars['engine'].'</p>';
+    echo '<p>'. 'Details:' . " " . $cars['details'].'</p>';
+    echo '<p>'. 'Summary: ' . " " . $cars['summary'].'</p>';
+    echo '<form action="" method="POST">';
+    echo '<input type="hidden" name="car_id" value="'.$cars['car_id'].'">';
+    echo '<input type="hidden" name="car_price" value="'.$cars['price'].'">';
+    echo '<input type="hidden" name="car_name" value="'.$cars['car_name'].'">';
+    
+    if (isset($_SESSION['loggedin'])) {
+        echo '<input type="submit" name="add_to_cart" value="Add to cart"/>';
+    }
+    
+    echo '</form>';
+    echo '</li>';
 }
 echo '</ul>';
 ?>
--- Roberts-Motors/websites/default/templates/shoppingcart.html.php
+++ Roberts-Motors/websites/default/templates/shoppingcart.html.php
@@ -1,15 +1,18 @@
 <?php
+
 session_start();
 
 $pdo = new PDO('mysql:dbname=roberts_motors;host=mysql', 'student', 'student');
-?>
-
-<h2>Shopping Cart</h2>
-
-<?php
-
-$LatestCars = $pdo->prepare('SELECT * FROM cars ORDER BY car_id ASC');
-$LatestCars->execute();
+$find = findAll($pdo, 'cart', 'car_id', 'car_name', 'price');
+$res1 = $pdo->prepare('SELECT sum(price) AS total FROM cart');
+$res1->execute();
+$total = 0;
+$row = $res1->fetch(PDO::FETCH_ASSOC);
+echo $row['total'];
 
 
+foreach($find as $cart){
+    echo '<p>'. 'Car Name:' . " " . $cart['car_name'].'</p>';
+    echo '<p>'. 'Car Price:' . " " . $cart['price'].'</p>';
+}
 ?>
