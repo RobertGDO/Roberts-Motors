485ea1170ffe9b75c21395768d3aee0b2e873452
RobertGDO <Robert.j.Allanofficial@gmail.com>
2024-05-17 22:10:28 +0100

    fixed addcar

--- Roberts-Motors/websites/default/public/AddCar.php
+++ Roberts-Motors/websites/default/public/AddCar.php
@@ -2,7 +2,7 @@
 
 require '../templates/loadtemplate.php';
 
-$output = loadtemplate('../templates/addcar.html.php', []);
+$output = loadtemplate('../templates/addcar.html.php',[]);
 
 echo loadtemplate('../templates/layout.html.php', [
 	'title' => 'Roberts Motors',
