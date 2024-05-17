cfbf2f4599a4b45f69111bec76d51e26f9436473
Queen-esty <126504317+Queen-esty@users.noreply.github.com>
2024-05-17 21:06:19 +0100

    fixed the layout

--- Roberts-Motors/websites/default/templates/layout2.html.php
+++ Roberts-Motors/websites/default/templates/layout2.html.php
@@ -9,7 +9,7 @@
     <link rel="shortcut icon" href="images/RM.png" type="image/x-icon">
     <link rel="stylesheet" href="main.css" media="screen">
     <link rel="stylesheet" href="mobile.css" media="screen and (max-width: 800px)">
-    <link rel="stylesheet" href="desktop.css" media="screen and (min-width: 800px)">
+    <link rel="stylesheet" href="desktop2.css" media="screen and (min-width: 800px)">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Eczar">
     <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> -->
