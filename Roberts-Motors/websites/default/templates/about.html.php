5480a5241ff013a1841766ea79229d946fcc7138
Queen-esty <126504317+Queen-esty@users.noreply.github.com>
2024-05-17 19:32:29 +0100

    added the about page , changed the search filter

---
+++ Roberts-Motors/websites/default/templates/about.html.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+  <main classs="aboutmain">
+        <div>
+            <h1>About Us </h1>
+            <p>
+
+                Welcome to roberts motors, where you find the best cars at the most reasonable prices,wheter you are
+                lokking for your first car or your next car robert motors is the best place for you,Since our opening in
+                1950 robert motors has been one of the top car dealers in the car industy, and it is still progressing
+                and growing to this point
+
+            </p>
+        </div>
+        <div>
+            <h2>
+                Our History 
+            </h2>
+            <p>
+                Robert motors was founded by the roberts family who had a passion for car,and had a dream to start selling cars,from a little beginning they have built roberts motors into 
+            </p>
+        </div>
+        <div>
+            <h2>
+                Design Decisions
+            </h2>
+            <p>
+                I decided to use a two-column grid template for all the pages on my website,I wanted an exciting colour
+                and not something boring and dull to look at first I went for a purple palette with dark purple as the
+                background colour at first it looked nice but the more I viewed it the uglier it looked so I used a
+                website called coolors and it generated a warm, attention-grabbing template that will not only be
+                calming to viewer but also exciting to see  https://coolors.co/u/fox_willow for fonts I used google
+                fonts https://fonts.google.com/specimen/Source+Code+Pro and choose the source code pro because it felt
+                interactive
+            </p>
+        </div>
+        <div>
+            <h2>
+                Conclusion
+            </h2>
+            <p>Although at some point I had some difficulties during this project with the help of the Lecturer and some
+                of my classmates I was able to finish project I am very proud of myself for how the project turned out
+                and cannot wait for my next step in Web Development </p>
+        </div>
+        <p>Word count;272</p>
+    </main>
